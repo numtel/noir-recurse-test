8db0a013512b1e708a14b963ff95193c81e423da
numtel <ben@latenightsketches.com>
2025-04-12 02:56:53 -0700

    Outer witness generates but not the proof

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { compile, createFileManager } from "@noir-lang/noir_wasm"
-import { UltraPlonkBackend } from '@aztec/bb.js';
+import { UltraHonkBackend } from '@aztec/bb.js';
 import { Noir } from '@noir-lang/noir_js';
 
 import initNoirC from "@noir-lang/noirc_abi";
@@ -7,19 +7,35 @@ import initACVM from "@noir-lang/acvm_js";
 import acvm from "@noir-lang/acvm_js/web/acvm_js_bg.wasm?url";
 import noirc from "@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm?url";
 
-import main from "./inner/src/main.nr?url";
-import nargoToml from "./inner/Nargo.toml?url";
+import innerMain from "./inner/src/main.nr?url";
+import innerNargoToml from "./inner/Nargo.toml?url";
+import outerMain from "./outer/src/main.nr?url";
+import outerNargoToml from "./outer/Nargo.toml?url";
 
 await Promise.all([initACVM(fetch(acvm)), initNoirC(fetch(noirc))]);
 
-export async function getCircuit() {
+async function getInnerCircuit() {
   const fm = createFileManager("/");
-  const { body } = await fetch(main);
-  const { body: nargoTomlBody } = await fetch(nargoToml);
+  const { body } = await fetch(innerMain);
+  const { body: nargoTomlBody } = await fetch(innerNargoToml);
 
   fm.writeFile("./src/main.nr", body);
   fm.writeFile("./Nargo.toml", nargoTomlBody);
   return await compile(fm);
+}
+
+async function getOuterCircuit() {
+  const fm = createFileManager("/");
+  const { body } = await fetch(outerMain);
+  const { body: nargoTomlBody } = await fetch(outerNargoToml);
+
+  fm.writeFile("./src/main.nr", body);
+  fm.writeFile("./Nargo.toml", nargoTomlBody);
+  return await compile(fm);
+}
+
+function foreignCallHandler() {
+  throw new Error('Unexpected foreign call');
 }
 
 const show = (id, content) => {
@@ -31,37 +47,71 @@ const show = (id, content) => {
 document.getElementById("submit").addEventListener("click", async () => {
   try {
     // noir goes here
-    show("logs", "Loading circuit... ⏳");
-    const { program } = await getCircuit();
-    const noir = new Noir(program);
-    const backend = new UltraPlonkBackend(program.bytecode, { threads: 8 }, { recursive: true });
-    show("logs", "Circuit loaded. ✅");
+    show("logs", "Loading inner circuit... ⏳");
+    const { program: innerProgram } = await getInnerCircuit();
+    const innerNoir = new Noir(innerProgram);
+    const innerBackend = new UltraHonkBackend(innerProgram.bytecode, { threads: 8 }, { recursive: true });
+    show("logs", "Inner circuit loaded. ✅");
 
-    show("logs", "Generating witness... ⏳");
-    const { witness } = await noir.execute({ x: 1 , y: 2 });
-    show("logs", "Generated witness. ✅");
+    show("logs", "Generating inner witness... ⏳");
+    const { witness: innerWitness } = await innerNoir.execute({ x: 1 , y: 2 });
+    show("logs", "Generated inner witness. ✅");
 
-    show("logs", "Generating proof... ⏳");
-    const { proof, publicInputs } = await backend.generateProof(witness);
-    show("logs", "Generated proof. ✅");
-    show("results", proof);
+    show("logs", "Generating inner proof... ⏳");
+    const {
+      proof: innerProof,
+      publicInputs: innerPublicInputs
+    } = await innerBackend.generateProof(innerWitness);
+    show("logs", "Generated inner proof. ✅");
 
-    const verified = await backend.verifyProof({ proof, publicInputs });
-    show("logs", `Verified ${verified}`);
+    // Not strictly necessary to do this here but for illustration
+    const innerVerified = await innerBackend.verifyProof({
+      publicInputs: innerPublicInputs,
+      proof: innerProof,
+    });
+    show("logs", `Inner proof verified: ${innerVerified}`);
 
     show("logs", "Generating recursive inputs... ⏳");
-    const publicInputsCount = 17;
-    const { proofAsFields, vkAsFields, vkHash } = await backend.generateRecursiveProofArtifacts( { publicInputs, proof }, publicInputsCount);
+    const recursiveProof = await innerBackend.generateProofForRecursiveAggregation(innerWitness);
+
+    const recursiveArtifacts = await innerBackend.generateRecursiveProofArtifacts(
+      {
+        publicInputs: innerPublicInputs,
+        proof: innerProof,
+      },
+      innerPublicInputs.length,
+    );
     show("logs", "Generated recursive inputs. ✅");
 
-    const recursiveInputs = {
-        verification_key: vkAsFields, // array of length 114
-        proof: proofAsFields, // array of length 93 + size of public inputs
-        publicInputs: [2], // using the example above, where `y` is the only public input
-        key_hash: vkHash,
+    show("logs", "Loading outer circuit... ⏳");
+    const { program: outerProgram } = await getOuterCircuit();
+    const outerNoir = new Noir(outerProgram);
+    const outerBackend = new UltraHonkBackend(outerProgram.bytecode, { threads: 8 }, { recursive: true });
+    show("logs", "Outer circuit loaded. ✅");
+
+    show("logs", "Generating outer witness... ⏳");
+    const outerInputs = {
+      public_inputs: recursiveProof.publicInputs,
+      key_hash: '0x0',
+      proof: recursiveProof.proof,
+      verification_key: recursiveArtifacts.vkAsFields,
+      z: '0xd00d',
     };
 
-    console.log(recursiveInputs);
+    const { witness: outerWitness } = await outerNoir.execute(
+      outerInputs,
+      foreignCallHandler,
+    );
+    show("logs", "Generated outer witness. ✅");
+
+    show("logs", "Generating outer proof... ⏳");
+    const {
+      proof: outerProof,
+      publicInputs: outerPublicInputs
+    } = await outerBackend.generateProof(outerWitness);
+    show("logs", "Generated outer proof. ✅");
+    show("results", outerProof);
+    console.log(outerProof, outerPublicInputs);
 
   } catch(error) {
     console.log(error);
