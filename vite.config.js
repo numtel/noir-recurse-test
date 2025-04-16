4828c5d83554cd0737f9271b9b4c496ddcd147a5
numtel <ben@latenightsketches.com>
2025-04-16 22:07:08 +0100

    vite config tabs to spaces

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	build: {
-		target: 'esnext',
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			target: 'esnext'
-		}
-	},
-	plugins: [
-		{
-			name: "configure-response-headers",
-			configureServer: (server) => {
-				server.middlewares.use((req, res, next) => {
-					res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-					res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-					next();
-				});
-			},
-		},
-	],
+  build: {
+    target: 'esnext',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+  plugins: [
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
 });
