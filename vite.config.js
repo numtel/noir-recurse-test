65d6136c1d58b08906ab2f524640ce9890829b84
numtel <ben@latenightsketches.com>
2025-04-16 21:11:19 +0100

    Update vite config for multi-threaded proving

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,24 @@
-export default { optimizeDeps: { esbuildOptions: { target: "esnext" } } };
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	build: {
+		target: 'esnext',
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			target: 'esnext'
+		}
+	},
+	plugins: [
+		{
+			name: "configure-response-headers",
+			configureServer: (server) => {
+				server.middlewares.use((req, res, next) => {
+					res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+					res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+					next();
+				});
+			},
+		},
+	],
+});
